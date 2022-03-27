a2a6281b8ae90ec9b405f02af64ff00ddcfc9598
Edwin Jorge <ed.jorge1122@gmail.com>
2022-03-27 17:26:34 -0500

    EDIT: grab movies

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ImgSlider } from '../ImgSlider/ImgSlider'
 import { Movies } from '../Movies/Movies'
 import { Viewers } from '../Viewers/Viewers'
 import { Container } from './Styled'
+import db from '../../firebase'
+import { useDispatch } from 'react-redux'
+import { setMovies } from '../../features/movie/movieSlice'
 
 export const Home = () => {
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    db.collection('movies').onSnapshot(snapshot => {
+      let tempMovies = snapshot.docs.map(doc => {
+        return { id: doc.id, ...doc.data() }
+      })
+      dispatch(setMovies(tempMovies))
+    })
+  }, [])
   return (
     <Container>
       <ImgSlider />
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Container, Content, Wrap } from './Styled'
+import { selectMovies } from '../../features/movie/movieSlice';
+import { useSelector } from 'react-redux';
 
 export const Movies = () => {
+  const movies = useSelector(selectMovies)
+
+  console.log('movies', movies)
   return (
     <Container>
       <h4>Recommended for you</h4>
       <Content>
+        {
+          movies && movies.map(movie => (
+            <Wrap key={movie.id}>
+              <img src={movie.cardImg} alt={movie.title} />
+              
+            </Wrap>
+          ))
+        }
         <Wrap>
           <img
             src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/0B2682B4B01548242036066A1428197E2CC6DB23135B43CEE90A854D4F1055F8/scale?width=800&aspectRatio=1.78&format=jpeg"
